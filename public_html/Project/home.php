fb82b92d2611fbc7b39f23b7d955464dea2f34f3
hwaneungsong <coryhsong@gmail.com>
2021-11-11 18:38:58 -0500

    fixes

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -5,6 +5,7 @@ require(__DIR__."/../../partials/nav.php");
 <?php
 if(is_logged_in(true)){
     flash("Welcome, " . get_username());
+    echo"<pre>" . var_export($_SESSION, true) . "<pre>";
 }
 ?>
 <?php
