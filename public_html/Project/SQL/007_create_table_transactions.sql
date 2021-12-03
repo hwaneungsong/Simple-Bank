6e0d2fa39d419e672e4176bd24b0cbcfc4bfdb93
hwaneungsong <coryhsong@gmail.com>
2021-12-03 02:26:59 -0500

    accounts table and transaction table

---
+++ public_html/Project/SQL/007_create_table_transactions.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS Transactions(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    src int,
+    dest int,
+    diff int,
+    reason varchar(15) not null COMMENT 'The type of transaction that occured',
+    memo varchar(240) default null COMMENT 'Any extra details to attach to the transaction',
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (src) REFERENCES Accounts(id),
+    FOREIGN KEY (dest) REFERENCES Accounts(id),
+    constraint ZeroTransferNotAllowed CHECK(diff != 0)
+)
---
+++ public_html/Project/SQL/009_insert_system_account.sql
@@ -0,0 +1,1 @@
+INSERT INTO Accounts(id, account, user_id, account_type) VALUES (-1, '000000000000', -1 , "world")
